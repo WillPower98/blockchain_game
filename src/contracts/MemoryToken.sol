90dcce61a51db2507ef4a943d1917ed0f69cd180
willmunoz <willdmunoz@gmail.com>
2022-02-16 21:13:02 -0500

    began tutorial

--- src/contracts/MemoryToken.sol
+++ src/contracts/MemoryToken.sol
@@ -3,6 +3,10 @@ pragma solidity ^0.5.0;
 import "./ERC721Full.sol";
 
 contract MemoryToken is ERC721Full {
-  // Code goes here...
 
+  string public name = "Memory Token";
+
+  constructor() ERC721Full(name, "MEMORY") public {
+    }
+    
 }
